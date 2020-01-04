53a8cc6e02c0b746f2ae1620fc13d53fe7c24da9
gu fan <gu.fan.work@gmail.com>
2020-01-04 21:59:26 +0800

    init

---
+++ src/renderer/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="wrapper">
+    Hello Clock
+  </div>
+</template>
+
+<script>
+  export default {
+  };
+</script>
+
+<style lang="stylus">
+  #wrapper
+    background black
+    color white
+</style>
